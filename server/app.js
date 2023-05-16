e532bd05b9a09e985ebbc72c1565924046f43e15
Zhetao <quzhetao2001@gmail.com>
2023-05-16 15:10:07 +0800

    Added server start up script

--- server/app.js
+++ server/app.js
@@ -1,12 +1,10 @@
 require("dotenv").config();require('dotenv').config();
 const express = require('express');
-const ejs = require('ejs');
 const app = express();
 const mongoose = require("mongoose");
 const passport = require("passport");
 const passportLocalMongoose = require("passport-local-mongoose");
 const session = require("express-session");
-const findOrCreate = require("mongoose-findorcreate");
 
 app.use(express.urlencoded({extended: true}));
 
