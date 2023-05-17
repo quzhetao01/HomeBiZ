05d3a7e923ddfb073dc5fcd9f9289f8d21982c18
Zhetao <quzhetao2001@gmail.com>
2023-05-17 16:09:01 +0800

    Added navigation

---
+++ src/pages/Home.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Home = () => {
+    return <div style={{backgroundColor: "red"}}>
+        <p>Home</p>
+    </div>
+}
+
+export default Home;
---
+++ src/pages/Login.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Login = () => {
+    return <div>
+        <p>Login</p>
+    </div>
+}
+
+export default Login;
