33cb88646ff768ce19cc31d9b3ab4a1cbf7e614c
Sheeepen <chengxi1020@gmail.com>
2023-06-20 16:27:37 +0800

    change navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,18 @@
+import React from 'react';
 import { NavLink } from 'react-router-dom';
-import NavbarCSS from "../styles/Navbar.css";
+import NavbarCSS from "../styles/Navbar.module.css";
 
 const Navbar = () => {
     return (
-        <nav className="navbar">
-            <h1>HomeBiZ</h1>
-            <div className="links">
-                <NavLink to='/'>Home</NavLink>
-                <NavLink to='/login'>Login</NavLink>
-                <NavLink to='/register'>Sign Up</NavLink>
+        <nav>
+            <div className={NavbarCSS.logo}>
+                <NavLink to='/'>
+                    <img src='http://localhost:3000/logo.png' alt="HomeBiZ logo" />
+                </NavLink>
+            </div>
+            <div className={NavbarCSS.links}>
+                <NavLink to='/'>View My Business</NavLink>
+                <NavLink to='/login'>Logout</NavLink>
             </div>
         </nav>
       );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -13,8 +13,7 @@ const Home = () => {
 
     const navigate = useNavigate();
 
-    return <div style={{backgroundColor: "red"}}>
-        <p>Home</p>
+    return <div>
         <button onClick={() => navigate("/createListing")}>Add Page</button>
         <button onClick={() => navigate("/viewListing", {state: {id: "648ecfa12f58d7b0dfa4c2c9"}}) }>View Listing Page</button>
 
