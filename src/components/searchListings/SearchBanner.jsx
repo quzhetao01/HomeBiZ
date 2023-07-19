fb189dded6307560972c6052303939e56c959ae9
Sheeepen <chengxi1020@gmail.com>
2023-07-20 00:48:39 +0800

    added category banners

--- src/components/searchListings/SearchBanner.jsx
+++ src/components/searchListings/SearchBanner.jsx
@@ -1,12 +1,27 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import CategoryListingsCSS from '../../styles/CategoryListings.module.css'
+import 'react-loading-skeleton/dist/skeleton.css';
+import Skeleton from 'react-loading-skeleton';
+
 
 const SearchBanner = () => {
-    return <div className="mb-4">
-      <img src="http://localhost:3000/1865.jpg" alt="banner" className={CategoryListingsCSS.image}/>
 
+
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setIsLoading(false);
+    }, 2000);
+    return () => clearTimeout(timeout);
+  }, [])
+
+  return (
+    <div className="mb-4">
+      {isLoading && <Skeleton className={CategoryListingsCSS.image} />}
+      {!isLoading && <img src="http://localhost:3000/others.jpg" alt="banner" className={CategoryListingsCSS.image}/>}
     </div>
-      
+  )    
 }
  
 export default SearchBanner;
